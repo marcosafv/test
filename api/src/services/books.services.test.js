01be043bcebc1e83c9ae1cdd08f37eadd68eb8ae
Marcos Flores <alejandro.visso@gmail.com>
2024-11-19 11:28:40 -0400

    fix: error to test api

--- api/src/services/books.services.test.js
+++ api/src/services/books.services.test.js
@@ -1,5 +1,19 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [{
+  _id: 1,
+  name: 'Book 1',
+}];
+
+// const MongoLibStub = {
+//   getAll: () => [...fakeBooks],
+//   create: jest.fn(),
+// };
+
+// jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({ getAll: () => [...fakeBooks], create: jest.fn() })));
+
 describe('Books Service', () => {
   let service = null;
   beforeEach(() => {
@@ -9,6 +23,7 @@ describe('Books Service', () => {
   // validate length of books
   it('should get books', async () => {
     const books = await service.getBooks();
+    console.log('Valor de books!!!', books);
     expect(books.length).toBeGreaterThan(0);
   });
 });
