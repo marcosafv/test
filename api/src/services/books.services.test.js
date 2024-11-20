f2e7d1060f4a38bf9c2aa94f422ed98ce60a0d7b
Marcos Flores <alejandro.visso@gmail.com>
2024-11-20 10:23:32 -0400

    chore: t

--- api/src/services/books.services.test.js
+++ api/src/services/books.services.test.js
@@ -25,6 +25,6 @@ describe('Books Service', () => {
   it('should get books', async () => {
     const books = await service.getBooks();
     console.log('Valor de books!!!', books);
-    expect(books.length).to.eq(0);
+    expect(books.length).toBe(0);
   });
 });
